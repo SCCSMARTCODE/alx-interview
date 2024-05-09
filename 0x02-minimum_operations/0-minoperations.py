65b3afdcbb5d6181975dff1e808ae0e18a91575a
SCCSMARTCODE <sccsmart247@gmail.com>
2024-05-09 20:15:04 +0100

    minimum_operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,50 +7,20 @@ H => Copy All => Paste => HH => Paste =>HHH
         => Copy All => Paste => HHHHHH => Paste => HHHHHHHHH
 Number of operations: 6
 """
-import typing
 
 
-def minOperations(n: int) -> int:
-    """
-    this is the minOperations function
-    :param n:
-    :return: operation_count
-    """
+def minOperations(target: int) -> int:
     operation_count = 0
-    current = 1
-    cursor = 1
-
-    while current != n:
-        if current > n:
-            return 0
-        cap_opr = copy_all_paste(current)
-        p_opr = paste(cursor, current)
-        if n % cap_opr == 0:
-            current = cursor = cap_opr
-            operation_count += 2
-            continue
-
-        cursor, current = p_opr
-        operation_count += 1
+    if target == 2:
+        return 2
+    if target > 1:
+        cursor = 2
+        while cursor <= target:
+            if target % cursor == 0:
+                operation_count += cursor
+                target = target/cursor
+                cursor = 2
+            if cursor >= target/2:
+                return int(operation_count + target)
+            cursor += 1
     return operation_count
-
-
-def copy_all_paste(current: int) -> int:
-    """
-    this is the copy_all & paste function
-    :param current:
-    :return: current
-    """
-    current *= 2
-    return current
-
-
-def paste(cursor: int, current: int) -> typing.Tuple[int, int]:
-    """
-    this is the paste function
-    :param cursor:
-    :param current:
-    :return: (cursor, new_current)
-    """
-    return cursor, current + cursor
-
