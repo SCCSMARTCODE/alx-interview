9d17a15c9e2c33b419cbeabb785ab5a9b8e2df4b
SCCSMARTCODE <sccsmart247@gmail.com>
2024-06-20 14:58:43 +0100

    run

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,41 +1,43 @@
 #!/usr/bin/python3
 """
-This is the function that solves the make change problem
+Making Change
 """
 
 
 def makeChange(coins, total):
     """
-    Calculates the minimum number of coins needed to make the total amount.
-
-    :param coins: List of coin denominations
-    :param total: Total amount to make
-    :return: Minimum number of coins needed
+    Return the minimum number of coins needed to meet a given total
+    Args:
+        coins (list of ints): a list of coins of different values
+        total (int): total value to be met
+    Return:
+        Number of coins or -1 if meeting the total is not possible
     """
     if total <= 0:
         return 0
-    return make_change_brain(coins, total)
+    if coins == [] or coins is None:
+        return -1
+    try:
+        n = coins.index(total)
+        return 1
+    except ValueError:
+        pass
 
-
-def make_change_brain(coins, total, count=0, least_count=None):
-    """
-    :param coins:
-    :param total:
-    :param count:
-    :param least_count:
-    :return: least_count
-    """
-    if least_count is None:
-        least_count = [total + 1]
-    if count >= least_count[0]:
-        return
-    for coin in coins:
-        if total >= 0 and total >= coin:
-            new_val = total - coin
-            if new_val == 0:
-                least_count[0] = min(least_count[0], count + 1)
-                return
-            elif new_val < 0:
-                return
-            make_change_brain(coins, new_val, count + 1, least_count)
-    return least_count[0]
+    coins.sort(reverse=True)
+    coin_count = 0
+    for i in coins:
+        if total % i == 0:
+            coin_count += int(total / i)
+            return coin_count
+        if total - i >= 0:
+            if int(total / i) > 1:
+                coin_count += int(total / i)
+                total = total % i
+            else:
+                coin_count += 1
+                total -= i
+                if total == 0:
+                    break
+    if total > 0:
+        return -1
+    return coin_count
